acffc6791f8e37a326dc17eb75595dc0f6c7ec92
Duyang0422 <113241673+Duyang0422@users.noreply.github.com>
2022-09-10 22:59:06 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -18,13 +18,13 @@ export const config = {
    * [
    *  {
    *    // 想要发送的人的名字
-   *    name: "杨桑0",
+   *    name: "老婆0",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "",
+   *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "",
+   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-   *    openUrl: ""
+   *    openUrl: "https://wangxinleo.cn"
    *   },
    * ]
    */
@@ -35,7 +35,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oLxQU6Zj09qkeo24sMucA55i46T8",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "bybr7jK4vtm_Dcb60CvXIDc6Fhjy2RwYeb7HzhJ7_QY",
+      useTemplateId: "9RceGNh8R78Z2xKUpgxGdQP6v7--ExyfGYtuvk73jpg",
       // 所在省份
       province: "陕西",
       // 所在城市
@@ -45,16 +45,63 @@ export const config = {
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://tophub.today/n/VaobJ98oAj",
+      openUrl: "https://s.weibo.com/top/summary?cate=realtimehot",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "杨桑": "小牛马", "year": "2003", "date": "06-20"},
-        {"type": "节日", "我们": "认识的时间", "year": "2019", "date": "09-05"},
+        {"type": "*生日", "杨": "牛马", "year": "2003", "date": "06-20"},
+        {"type": "节日", "咱": "认识", "year": "2019", "date": "09-05"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "牛牛": "阳桑", "year": "2002", "date": "04-22"},
-        {"type": "节日", "杨桑": "最近找我唠嗑的时间", "year": "2022", "date": "09-09"},
+        {"type": "生日", "杜": "帅比", "year": "2002", "date": "04-22"},
+        {"type": "节日", "小牛马": "最近找我唠嗑的时间", "year": "2022", "date": "09-09"},
       ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 在一起的日子
+        {"keyword": "love_day", date: "2022-09-08"},
+        // 结婚纪念日
+        {"keyword": "marry_day", date: "2022-09-09"},
+        // 退伍日
+        {"keyword": "ex_day", date: "2022-09-10"},
+      ]
+    },
+    {
+      name: "老婆1",
+      id: "",
+      useTemplateId: "",
+      province: "",
+      city: "",
+      horoscopeDate: '',
+      horoscopeDateType: '',
+      openUrl: "https://wangxinleo.cn",
+      festivals: [],
+      customizedDateList: []
+    },
+    {
+      name: "老婆2",
+      id: "",
+      useTemplateId: "",
+      province: "",
+      city: "",
+      horoscopeDate: '',
+      horoscopeDateType: '',
+      openUrl: "https://wangxinleo.cn",
+      festivals: [],
+      customizedDateList: []
+    },
+    {
+      name: "老婆3",
+      id: "",
+      useTemplateId: "",
+      province: "",
+      city: "",
+      horoscopeDate: '',
+      horoscopeDateType: '',
+      openUrl: "https://wangxinleo.cn",
+      festivals: [],
+      customizedDateList: []
+    },
+  ],
 
   /**
    * 【推送完成提醒】 相关，主要用来展示发送是否成功/失败的数据
@@ -80,9 +127,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "陕西",
+  PROVINCE: "广东",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "宝鸡",
+  CITY: "惠州",
 
   /** 重要节日相关 */
 
@@ -140,14 +187,12 @@ export const config = {
     {"keyword": "encourage_oneself", contents: "你主要的问题在于读书太少而想得太多"},
     // 这样配置的话，就会每次随机选一句话发送
     {"keyword": "lover_prattle", contents: [
-      "寒来暑往秋收冬藏，希望我们来日方长",
+     "寒来暑往秋收冬藏，希望我们来日方长",
       "万里星光 一如既往。",
       "慢慢来，谁还没有一个努力得过程",
       "可以不万丈光芒，但不要停止自己发光",
       "你坚持的东西总有一天会反过来拥抱你",
        "永远热泪盈眶，永远积极向上",
-      
-     
     ]},
   ],
 
@@ -155,7 +200,7 @@ export const config = {
 
   // 好文节选的内容类型
   // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-  LITERARY_PREFERENCE: "https://music.163.com/#/song?id=1345353903"
+  LITERARY_PREFERENCE: ""
 
 
 }
